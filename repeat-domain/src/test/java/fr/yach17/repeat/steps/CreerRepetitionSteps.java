971cff78ddf47903f3c3bac86d03b395b2a8b50a
yach17 <yannick.chazeau@gmail.com>
2014-07-21 08:38:54 +0200

    renamed Create Repetition Feature to Repetition Next Date and changed steps method to english name

--- repeat-domain/src/test/java/fr/yach17/repeat/steps/CreerRepetitionSteps.java
+++ repeat-domain/src/test/java/fr/yach17/repeat/steps/CreerRepetitionSteps.java
@@ -25,49 +25,49 @@ public class CreerRepetitionSteps {
     }
 
     @Given("^I have a yearly repetition$")
-    public void que_j_ai_une_répétition_annuelle() throws Throwable {
+    public void createYearlyRepetition() throws Throwable {
         repetition = new PeriodicRepetition(Period.ofYears(1));
     }
 
     @When("^I set the start date to today$")
-    public void je_définis_sa_date_de_début_à_aujourd_hui() throws Throwable {
+    public void setStartDateToToday() throws Throwable {
         repetition.setStartDate(LocalDate.now());
     }
 
     @Then("^the next date is next year$")
-    public void la_date_suivante_est_l_an_prochain() throws Throwable {
+    public void checkNextDateIsNextYear() throws Throwable {
         assertThat(repetition.getNextDate()).isEqualTo(LocalDate.now().plusYears(1));
 
     }
 
     @Given("^I have a monthly repetition$")
-    public void que_j_ai_une_répétition_mensuelle() throws Throwable {
+    public void createMonthlyRepetition() throws Throwable {
         repetition = new PeriodicRepetition(Period.ofMonths(1));
     }
 
     @Then("^the next date is next month$")
-    public void la_date_suivante_est_le_mois_prochain() throws Throwable {
+    public void checkNextDateIsNextMonth() throws Throwable {
         assertThat(repetition.getNextDate()).isEqualTo(LocalDate.now().plusMonths(1));
     }
 
     @Given("^I have a repetition with a period of (\\d+) days$")
-    public void que_j_ai_une_répétition_de_période_days(int nbJours) throws Throwable {
+    public void createPeriodicRepetition(int nbJours) throws Throwable {
         repetition = new PeriodicRepetition(Period.ofDays(nbJours));
     }
 
 
     @When("^I set the start date to \"([^\"]*)\"$")
-    public void je_définis_sa_date_de_début_au(String dateString) throws Throwable {
+    public void setStartDate(String dateString) throws Throwable {
         repetition.setStartDate(StepsHelper.parse(dateString));
     }
 
     @Then("^the next date is \"([^\"]*)\"$")
-    public void la_date_suivante_est_le(String dateExpected) throws Throwable {
+    public void checkNextDate(String dateExpected) throws Throwable {
         assertThat(repetition.getNextDate()).isEqualTo(StepsHelper.parse(dateExpected));
     }
 
     @Given("^I have an end of month repetition (\\d+) days before the end of the month$")
-    public void que_j_ai_une_répétition_de_fin_de_mois_jours_avant_la_fin_du_mois(int nbJoursAvantFinDuMois) throws Throwable {
+    public void createEndOfMonthRepetition(int nbJoursAvantFinDuMois) throws Throwable {
         repetition = new EndOfMonthRepetition(nbJoursAvantFinDuMois);
     }
 
